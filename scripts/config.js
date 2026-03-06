2c5f149c6f40475684f54244dec8ca4307f28ce5
Manu <jlopez221104@gmail.com>
2026-03-06 17:50:59 -0600

    site 50%

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,8 @@
 /*================
     URL GLOBAL
 ================*/
-export const API_BASE_URL = "http://localhost:8080/api";
-
+//export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = "https://granja-1ryk.onrender.com/api";
 //PROTOTIPE
 export async function fetchJson(endpoint) {
     const response = await fetch(`${API_BASE_URL}${endpoint}`);
